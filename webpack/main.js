2408743d6e0587083a2e36217a29b7a25a1b891b
bexaca <33089360+bexaca@users.noreply.github.com>
2017-11-12 21:36:55 +0100

    Initial commit

---
+++ webpack/main.js
@@ -0,0 +1,13 @@
+//BOOTSTRAP JS
+import Bootstrap from "bootstrap";
+
+//CUSTOM JS
+
+require ('./jsFiles/jquery.nicescroll.js');
+require ('./jsFiles/custom.js');
+require ('./jsFiles/classie.js');
+require ('./jsFiles/slick.min.js');
+require ('./jsFiles/anime.js');
+require ('./jsFiles/replace_me.js');
+
+//EXTERNAL JS
